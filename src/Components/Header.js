5cb0f330b5df3ea1cf3f30ba770c2a6d92cf889d
moayadalhaj <moayad.alhaj21@gmail.com>
2021-08-02 17:28:38 +0300

    view the images on the page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <main>
+      <header>
         <h1>Horned Animals</h1>
-      </main>
+      </header>
     );
   }
 }
